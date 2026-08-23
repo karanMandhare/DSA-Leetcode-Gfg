ee6fc06609c838a76b20edc916ef9fb7f52a6342
Karan Mandhare <hshhh496@gmail.com>
2026-08-23 16:09:02 +0530

    Time: 15 ms (35.48%), Space: 12.4 MB (56.06%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        s=str(x)
+        if s ==s [::-1]:
+              return True
+        else:
+            return False
+            
+      
+
+   
+
+
+
+
+        
+
+
+       
+        
